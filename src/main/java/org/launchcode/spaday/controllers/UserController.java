970873859737d1407e6f0a5cfcab1bdd80380986
Rita Clark-Browning <ritaclark-browning@iMac-607.local>
2022-10-25 17:57:27 -0500

    Completed Spa Day Studio 1

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,27 @@
+package org.launchcode.spaday.controllers;
+
+import org.launchcode.spaday.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Controller
+@RequestMapping(value = "user", method = {RequestMethod.GET, RequestMethod.POST})
+public class UserController {
+
+    @GetMapping("add")
+    public String displayAddUserForm() {
+        return "user/add";
+    }
+
+    @PostMapping()
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        model.addAttribute("user", user);
+            return "user/index";
+    }
+}
+
