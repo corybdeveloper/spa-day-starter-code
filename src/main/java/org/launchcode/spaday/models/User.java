e081bc2881e8b998904a911d1778c40ad69f712b
Rita Clark-Browning <ritaclark-browning@iMac-607.local>
2022-10-27 19:22:45 -0500

    Studio Finished not including bonus

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,21 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotBlank
+    @Size(min = 5, max = 15)
     private String username;
+    @Email
     private String email;
+    @NotBlank
+    @Size(min = 6)
     private String password;
+    @NotBlank
+    private String verify;
 
     public User() {
 
@@ -38,5 +50,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
     }
 }
