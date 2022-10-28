a4008a0e8d5aa2b71cc6efeb3d6f960cf4713372
Rita Clark-Browning <ritaclark-browning@iMac-607.local>
2022-10-27 21:24:29 -0500

    error class

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -19,14 +19,9 @@ public class UserController {
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors) {
         model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        if (!user.getPassword().equals(verify)) {
-            model.addAttribute("error", "Passwords do not match");
-            return "user/add";
-        } else if (errors.hasErrors()) {
-            model.addAttribute("error", "Invalid Data");
+        if (errors.hasErrors()) {
             return  "user/add";
         }
             return "user/index";
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,6 @@
 package org.launchcode.spaday.models;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 public class User {
     @NotBlank
@@ -12,10 +9,10 @@ public class User {
     @Email
     private String email;
     @NotBlank
-    @Size(min = 6)
+    @Size(min = 6, max = 250)
     private String password;
-    @NotBlank
-    private String verify;
+    @NotNull(message = "Passwords do not match.")
+    private String verifyPassword;
 
     public User() {
 
@@ -26,6 +23,12 @@ public class User {
         this.username = username;
         this.email = email;
         this.password = password;
+    }
+
+    private void checkPassword() {
+        if (password != null && verifyPassword != null && !password.equals(verifyPassword)) {
+            setVerifyPassword(null);
+        }
     }
 
     public String getUsername() {
@@ -50,13 +53,15 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
     }
 
-    public String getVerify() {
-        return verify;
+    public String getVerifyPassword() {
+        return verifyPassword;
     }
 
-    public void setVerify(String verify) {
-        this.verify = verify;
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
     }
 }
